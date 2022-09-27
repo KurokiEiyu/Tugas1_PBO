0899f24685af2777a4db6bb93ab82d613fd346b8
KurokiEiyu <114289741+KurokiEiyu@users.noreply.github.com>
2022-09-27 15:01:34 +0700

    Add files via upload

---
+++ PBO1sesi1.java
@@ -0,0 +1,7 @@
+public class PBO1sesi1 {
+    public static void main(String[] args){
+        System.out.println("Belajar JAVA");
+        System.out.println("sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
---
+++ PBO2sesi1.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+class PBO2sesi1{
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null,"Belajar "+ input +" sangat mudah");
+    }
+}
